66571d1eaad913b47188067c31e7b151c1905e4a
jonasac <jonasac@student.matnat.uio.no>
2013-02-24 16:35:06 +0100

    add tmux config

--- boostrap.sh
+++ boostrap.sh
@@ -3,5 +3,6 @@ ln -s ~/.dotfiles/bashrc    ~/.bashrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vim       ~/.vim
 ln -s ~/.dotfiles/bin   ~/.bin
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/emacs ~/.emacs.d
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
