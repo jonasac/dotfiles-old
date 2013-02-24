e32f16b6b61d1654cf907cbf3e4e4828a7fff896
jonasac <jonasac@student.matnat.uio.no>
2013-02-24 14:33:48 +0100

    update bootstrap.sh

--- boostrap.sh
+++ boostrap.sh
@@ -2,5 +2,6 @@ ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/bashrc    ~/.bashrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vim       ~/.vim
-ln -s ~/.dotfiles/bin/	    ~/.bin
+ln -s ~/.dotfiles/bin   ~/.bin
+ln -s ~/.dotfiles/emacs ~/.emacs.d
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
