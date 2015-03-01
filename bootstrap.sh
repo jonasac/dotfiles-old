3929f983372078922a1d97967857ee62e8b757df
Jonas Arnold Clasen <jonas.clasen@gmail.com>
2015-03-01 22:00:28 +0100

    cleanup and move to stow

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/bashrc    ~/.bashrc
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/vim       ~/.vim
-ln -s ~/.dotfiles/bin   ~/.bin
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/emacs ~/.emacs.d
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
---
+++ zsh/.zshrc
@@ -0,0 +1,13 @@
+source ~/.dotfiles/tools/antigen.zsh
+autoload -U promptinit && promptinit
+autoload -U compinit && compinit
+
+antigen-use oh-my-zsh
+antigen theme alanpeabody
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle colored-man
+
+if [[ `uname` == 'Darwin' ]]
+then
+	export PATH=/usr/local/bin:$PATH
+fi
